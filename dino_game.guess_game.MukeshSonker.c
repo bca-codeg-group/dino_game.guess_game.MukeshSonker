f2a6eba5c09659058204c2e506461dbe0d2945e9
LaurencePinto <138968373+LaurencePinto@users.noreply.github.com>
2023-07-08 14:22:51 +0530

    Add files via upload

---
+++ dino_game.guess_game.MukeshSonker.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+#define WIDTH 30
+#define HEIGHT 10
+
+int dinoX, dinoY;
+int obstacleX;
+int score;
+
+void initialize() {
+    dinoX = 5;
+    dinoY = HEIGHT - 1;
+    obstacleX = WIDTH - 1;
+    score = 0;
+}
+
+void draw() {
+    system("cls");
+
+    for (int i = 0; i < WIDTH; i++) {
+        printf("-");
+    }
+    printf("\n");
+
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            if (i == dinoY && j == dinoX) {
+                printf("D");
+            } else if (i == HEIGHT - 1 && j == obstacleX) {
+                printf("O");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < WIDTH; i++) {
+        printf("-");
+    }
+    printf("\n");
+
+    printf("Score: %d\n", score);
+}
+
+void update() {
+    if (obstacleX == 0) {
+        score++;
+        obstacleX = WIDTH - 1;
+    } else {
+        obstacleX--;
+    }
+
+    if (kbhit()) {
+        char key = getch();
+        if (key == ' ') {
+            if (dinoY == HEIGHT - 1) {
+                dinoY -= 2;
+            }
+        }
+    }
+
+    if (dinoY < HEIGHT - 1) {
+        dinoY++;
+    }
+}
+
+int main() {
+    initialize();
+
+    while (1) {
+        draw();
+        update();
+    }
+
+    return 0;
+}
